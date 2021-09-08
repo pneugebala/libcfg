9c5829dfaab5a3c2fd206d44e1276bc8ef2b4f77
Andreas Neugebauer <pneugebala@gmail.com>
2021-09-08 22:43:01 +0200

    feat: extend example

--- example/example.c
+++ example/example.c
@@ -7,6 +7,20 @@ int main(void) {
   LibCfgRoot* cfg = libcfg_read(read_path, 0);
   int last_error = libcfg_get_last_error();
 
+  cfg->entries[0].value = "2";
+  cfg->entries[1].value = "Hello";
+
+  libcfg_add_entry(cfg, "is_new", "true");
+  libcfg_remove_entry(cfg, &cfg->entries[1]);
+
+  libcfg_add_section_entry(&cfg->sections[1], "is_new", "true");
+  libcfg_remove_section_entry(&cfg->sections[1], &cfg->sections[1].entries[1]);
+
+  libcfg_add_section(cfg, "Section 3");
+  libcfg_remove_section(cfg, &cfg->sections[0]);
+
+  libcfg_add_section_entry(&cfg->sections[1], "is_new", "N/A");
+
   libcfg_write(write_path, cfg);
   last_error = libcfg_get_last_error();
 
